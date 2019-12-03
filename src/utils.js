000ff67b9d7ebc995acf98aabf49f7f74e09b3e4
Andrey Belyaev <ianbel263@gmail.com>
2019-12-03 13:37:12 +0300

    Переносит словарь месяцев в utils

--- src/utils.js
+++ src/utils.js
@@ -11,3 +11,18 @@ export const formatDate = (currentDate) => {
 
   return `${date}/${month}/${year} ${hours}:${minutes}`;
 };
+
+export const valueToMonth = {
+  0: `JAN`,
+  1: `FEB`,
+  2: `MAR`,
+  3: `APR`,
+  4: `MAY`,
+  5: `JUN`,
+  6: `JUL`,
+  7: `AUG`,
+  8: `SEP`,
+  9: `OCT`,
+  10: `NOV`,
+  11: `DEC`
+};
