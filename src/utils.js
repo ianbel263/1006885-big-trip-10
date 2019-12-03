83bd942618990aa5b39bb80902fabe52adec7ccd
Andrey Belyaev <ianbel263@gmail.com>
2019-12-03 16:46:57 +0300

    Исправляет функцию форматирования даты

--- src/utils.js
+++ src/utils.js
@@ -5,7 +5,7 @@ const castTimeFormat = (value) => {
 export const formatDate = (currentDate) => {
   const date = castTimeFormat(currentDate.getDate());
   const month = castTimeFormat(currentDate.getMonth() + 1);
-  const year = currentDate.getFullYear() - 2000;
+  const year = castTimeFormat(currentDate.getFullYear() % 100);
   const hours = castTimeFormat(currentDate.getHours());
   const minutes = castTimeFormat(currentDate.getMinutes());
 
