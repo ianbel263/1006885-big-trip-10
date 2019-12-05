237bb49856a2220404de39282d0823b89856f686
Andrey Belyaev <ianbel263@gmail.com>
2019-12-05 13:36:37 +0300

    Заменяет карточку

--- src/components/event-edit.js
+++ src/components/event-edit.js
@@ -3,9 +3,10 @@ import {formatDate} from '../utils.js';
 export const createEventEditFormTemplate = (event, eventPointTypes, eventPointCities) => {
 
   const {destination, startDate, endDate, price, offers, description, photosUrls} = event;
+  console.log("offers", offers);
 
   return (
-    `<form class="trip-events__item  event  event--edit" action="#" method="post">
+    `<form class="event  event--edit" action="#" method="post">
       <header class="event__header">
         <div class="event__type-wrapper">
           <label class="event__type  event__type-btn" for="event-type-toggle-1">
@@ -24,7 +25,6 @@ export const createEventEditFormTemplate = (event, eventPointTypes, eventPointCi
 
             ${eventPointTypes.filter((eventType) => eventType.group === el)
               .map(({type}) => {
-                // console.log("it", it);
                 return (
                   `<div class="event__type-item">
                     <input id="event-type-${type}-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${type}">
@@ -35,16 +35,17 @@ export const createEventEditFormTemplate = (event, eventPointTypes, eventPointCi
             </fieldset>`
         );
       }).join(`\n`)}
+
           </div>
         </div>
 
         <div class="event__field-group  event__field-group--destination">
           <label class="event__label  event__type-output" for="event-destination-1">
-      ${event.type.type} at
+            ${event.type.type} at
           </label>
           <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${destination}" list="destination-list-1">
           <datalist id="destination-list-1">
-      ${eventPointCities.map((city) => {
+    ${eventPointCities.map((city) => {
       return (
         `<option value="${city}"></option>`
       );
@@ -73,14 +74,28 @@ export const createEventEditFormTemplate = (event, eventPointTypes, eventPointCi
         </div>
 
         <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
-        <button class="event__reset-btn" type="reset">Cancel</button>
+        <button class="event__reset-btn" type="reset">Delete</button>
+
+        <input id="event-favorite-1" class="event__favorite-checkbox  visually-hidden" type="checkbox" name="event-favorite" checked>
+        <label class="event__favorite-btn" for="event-favorite-1">
+          <span class="visually-hidden">Add to favorite</span>
+          <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
+            <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
+          </svg>
+        </label>
+
+        <button class="event__rollup-btn" type="button">
+          <span class="visually-hidden">Open event</span>
+        </button>
       </header>
+
       <section class="event__details">
 
         <section class="event__section  event__section--offers">
           <h3 class="event__section-title  event__section-title--offers">Offers</h3>
 
           <div class="event__available-offers">
+
     ${offers.map(({type, price: offerPrice, title, isChecked}) => {
       return (
         `<div class="event__offer-selector">
@@ -93,6 +108,7 @@ export const createEventEditFormTemplate = (event, eventPointTypes, eventPointCi
         </div>`
       );
     }).join(`\n`)}
+
           </div>
         </section>
 
