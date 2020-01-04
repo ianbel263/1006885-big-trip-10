c91d4abb4ec1189a6bd078e9349884197115587b
Andrey Belyaev <ianbel263@gmai.com>
2020-01-04 11:37:59 +0300

    1

--- src/const.js
+++ src/const.js
@@ -1,4 +1,4 @@
-export const AUTHORIZATION = `Basic kdnwdfskNffCKjkj-j`;
+export const AUTHORIZATION = `Basic kdnwdfskNdgffCKjkj-j`;
 export const END_POINT = `https://htmlacademy-es-10.appspot.com/big-trip`;
 
 export const RequestMethod = {
